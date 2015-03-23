eb0bc133bbd6ece63461f698a2ea888cf4a16a70
Matt Green <matt.green@mead354.org>
2015-03-23 11:16:04 -0700

    added comments

--- buttonTester/buttonTester.ino
+++ buttonTester/buttonTester.ino
@@ -1,21 +1,27 @@
 
+// setup your circuit to match.
 int buttonPin = 2;
 int led = 13;
+
+// create variable to hold if the "state" of the button, is it being pressed or not.
 int buttonState = 0;
-int ledOn = 0;
 
 void setup()
 {
   pinMode(buttonPin, INPUT); 
   pinMode(led, OUTPUT);
+  
+  // optional -- used with serial monitor to test
   Serial.begin(9600);
 }
 
 void loop()
 {
  
+  // get a reading off the input pin
   buttonState = digitalRead(buttonPin);
   
+  // if you are pressing the button
  if (buttonState == 1) {
   Serial.println("ON");
  digitalWrite(led, HIGH);  
