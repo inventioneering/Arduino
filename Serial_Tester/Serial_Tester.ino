dc3b238d6e308f1139358cbddfc3d1b6a34d11d7
Matthew Green <matt.green@mead354.org>
2016-01-20 08:54:37 -0800

    Revert "Merge pull request #1 from riverpointacademy/add-blinking-led-code"
    This reverts commit 0a6f07b2172dbdb38f674d48b7221ceddb3987ac, reversing
    changes made to 65e4ae8e8a422007778ed124a8b2c227d459b920.

--- Serial_Tester/Serial_Tester.ino
+++ Serial_Tester/Serial_Tester.ino
@@ -2,20 +2,14 @@
 void setup() {
   // start serial port at 9600 bps:
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
  
 }
 
 void loop() {
   // plug into A0 and see what happens
-    Serial.print(analogRead(A0));
+    Serial.print(analogRead(A1));
     Serial.print("\n");
-    //delay(50);
-    
-    // this makes the LED light up with the value from the sensor 
-    // divided by 4 to map the values from 0 - 255
-    analogWrite(9, analogRead(A0)/4);
-    
+    delay(200);
  
 }
 
