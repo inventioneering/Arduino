8da863315a2d8079be7176eca87b27870a1a1aba
Matt Green <Matt.Green@mead354.org>
2018-10-15 10:57:28 -0700

    updates

--- RCDataLogger/rcDataLogger-v3/PitotTest1/PitotTest1.ino
+++ RCDataLogger/rcDataLogger-v3/PitotTest1/PitotTest1.ino
@@ -1,47 +1,178 @@
+
+
 /*
-  Analog input, analog output, serial output
+  SD card datalogger
 
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
+ This example shows how to log data from three analog sensors
+ to an SD card using the SD library.
 
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
 
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
+ created  24 Nov 2010
+ modified 9 Apr 2012
+ by Tom Igoe
 
-  This example code is in the public domain.
+ This example code is in the public domain.
 
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-*/
+ */
 
-// These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
 
-int sensorValue = 0;        // value read from the pot
+String fileName;
 
+
+
+// Power by connecting Vin to 3-5V, GND to GND
+// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
+// See the Wire tutorial for pinouts for each Arduino
+// http://arduino.cc/en/reference/wire
+//Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+const int chipSelect = 4;
 
 void setup() {
-  // initialize serial communications at 9600 bps:
+  //randomSeed(analogRead(0));
+  //randomer = String(random(1,100));
+  //fileName = "rc_" + randomer + ".txt";
+  fileName = "rc.txt";
+  
+  pinMode(13, OUTPUT);
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
+
+  if(Serial) {
+    Serial.print("Initializing SD card...");
+  }
+  
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    if(Serial) {
+      Serial.println("Card failed, or not present");
+    }
+    
+    // don't do anything more:
+    return;
+  }
+  if(Serial) {
+    Serial.println("card initialized.");
+    //Serial.println("Adafruit_MPL3115A2 test!");
+  }
+  
+
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  String tabCharacter = "\t";
+  String header = "Airspeed";
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(tabCharacter);
+    dataFile.println(header);
+    dataFile.close();
+    // print to the serial port too:
+    if(Serial) {
+       Serial.println(header);
+    }
+   
+  }
 }
 
 void loop() {
-  // read the analog in value:
-  sensorValue = analogRead(analogInPin);
+  
+  
+//  if (! baro.begin()) {
+//    if(Serial) {
+//       Serial.println("Couldnt find sensor");
+//       Serial.println("Starting time in seconds: " + millis()/1000);
+//    }
+//     
+//      return;
+//    }
 
-  // print the results to the Serial Monitor:
-  if(Serial) {
-     Serial.print("sensor = ");
-     Serial.print(sensorValue);
-     Serial.print("\n");
-  }
+      // make a string for assembling the data to log:
+     String dataString = "";
+
+     //float pascals = baro.getPressure();
+  // Our weather page presents pressure in Inches (Hg)
+  // Use http://www.onlineconversion.com/pressure.htm for other units
+ // Serial.print(pascals/3377); Serial.println(" Inches (Hg)");
+
+ // float altm = baro.getAltitude();
+  //Serial.print(altm); Serial.println(" meters");
+
+ // float tempC = baro.getTemperature();
+  //Serial.print(tempC); Serial.println("*C");
+
+ // delay(50);
  
 
-  delay(2);
+
+  // read three sensors and append to the string:
+//  for (int analogPin = 0; analogPin < 3; analogPin++) {
+//    int sensor = analogRead(analogPin);
+//    dataString += String(sensor);
+//    if (analogPin < 2) {
+//      dataString += ",";
+//    }
+//  }
+
+  // put data in string formatted, "airspeed"
+  float reading = analogRead(0);
+  float pressure = map(reading,0,1023,0,5);
+  int VS = 5; // input voltage
+  // Vout = VS × (0.2 × P(kPa)+0.5) ± 6.25% VFSS
+  float vOut = VS *(0.2*reading+0.5);
+  dataString += vOut;
+  if(Serial) {
+    //Serial.println(vOut);
+    Serial.println(vOut);
+  }
+
+  
+  digitalWrite(13, HIGH);
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  
+  
+  File dataFile = SD.open(fileName, FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    if(Serial) {
+       //Serial.println(dataString);
+    }
+    digitalWrite(13, LOW);
+    delay(50);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    if (Serial) {
+      Serial.println("error opening datalog.txt");
+    }
+    
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
